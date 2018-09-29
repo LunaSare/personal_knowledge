e40cca611e914f60e1a75923d54557d1682dae8e
Luna Luisa Sanchez Reyes <sanchez.reyes.luna@gmail.com>
2018-09-28 23:29:02 -0400

    Create server.R

---
+++ server.R
@@ -0,0 +1,11 @@
+ui <- pageWithSidebar(
+
+  # App title ----
+  headerPanel("Personal Knowledge Cloud"),
+
+  # Sidebar panel for inputs ----
+  sidebarPanel(),
+
+  # Main panel for displaying outputs ----
+  mainPanel()
+)
